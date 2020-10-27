8cd68c3219bccde836df1eda0f4f9a056991b0a7
guil24 <guillermo.maestre@hotmail.com>
2020-10-26 22:06:02 -0300

    master

---
+++ app/src/main/java/com/gmaestre/curso3_mascotas_s4/Constants.java
@@ -0,0 +1,6 @@
+package com.gmaestre.curso3_mascotas_s4;
+
+public class Constants {
+    public static final String email = "test@gmail.com";
+    public static final String password = "123456";
+}
